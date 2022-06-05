80af80219328196bd611ec5575d4dc7351431bbc
ungespurv <radoslawwroblewski@yahoo.pl>
2022-06-05 19:40:10 +0200

    The logic of the functions has been changed, and vectorization has been added. Still, the output is incorrect.

--- functions.py
+++ functions.py
@@ -1,26 +1,33 @@
+import numpy as np
+
+
+def a_v_map_creation(first_frequency_map, comparison_map, standard_deviation):
+    """Scales a map to a certain frequency"""
+    tmp_list = first_frequency_map
+    for row in range(len(first_frequency_map)):
+        for value in range(len(first_frequency_map[0])):
+            if comparison_map[row][value] == 0 or comparison_map[row][value] < 2 * standard_deviation:
+                tmp_list[row][value] = 1
+            elif first_frequency_map[row][value] / comparison_map[row][value] < 2.86:
+                tmp_list[row][value] = 2.86
+            else:
+                tmp_list[row][value] = ((first_frequency_map[row][value] / comparison_map[row][value]) / 2.86) ** 2.114
+    tmp_list = np.apply_along_axis(lambda x: 2.5 * np.log10(x), axis=0, arr=tmp_list)
+    return tmp_list
+
+
 def k_lambda(wavelength):
     """Calculates the k(lambda) value for each wavelength"""
     r_v = 4.05
     if wavelength < 630:
         return 2.659 * (1.509 / wavelength - 0.198 / wavelength ** 2 - 0.011 / wavelength ** 3) + r_v
     else:
-        return 2.659 * (-1.857 + 1.040/wavelength) + r_v
+        return 2.659 * (-1.857 + 1.040 / wavelength) + r_v
 
 
-def a_v_map_creation(first_frequency_map, corrected, comparison_map, standard_deviation):
-    """Scales a map to a certain frequency"""
-    for pixel1 in range(334):
-        for pixel2 in range(331):
-            if comparison_map[pixel1][pixel2] == 0 or comparison_map[pixel1][pixel2] < 2 * standard_deviation:
-                corrected.append(1)
-            elif first_frequency_map[pixel1][pixel2] / comparison_map[pixel1][pixel2] < 2.86:
-                corrected.append(2.86)
-            else:
-                corrected.append((first_frequency_map[pixel1][pixel2] / comparison_map[pixel1][pixel2] / 2.86) ** 2.114)
-
-
-def a_lambda_calculation(a_v_map, k_lambda_for_specific_wavelength, a_lambda_output_map):
+def a_lambda_calculation(a_v_map, k_lambda_for_specific_wavelength):
     """Calculates A_v map to A_lambda map"""
     r_v = 4.05
-    for i, pixel in enumerate(a_v_map):
-        a_lambda_output_map.append((k_lambda_for_specific_wavelength * a_v_map[i]) / r_v)
+    tmp_list = a_v_map
+    tmp_list = np.apply_along_axis(lambda x: (k_lambda_for_specific_wavelength * x) / r_v, axis=0, arr=tmp_list)
+    return tmp_list
---
+++ main.py
@@ -0,0 +1,35 @@
+from astropy.io import fits
+import functions
+# 332 x 335
+
+H_alfa = fits.getdata('AT2018cow_Ha.fits')  # 656.46 nm II
+H_beta = fits.getdata('AT2018cow_Hb.fits')  # 486.1 nm I
+N_II = fits.getdata('AT2018cow_NII.fits')  # 648.4 nm II
+O_III = fits.getdata('AT2018cow_OIII.fits')  # 500.7nm I
+S_II = fits.getdata('AT2018cow_SII.fits')  # 671.731  II
+emission_wavelengths = [656.46, 486.1, 648.4, 500.7, 671.731]
+
+H_alfa_standard_deviation = 13.507
+H_beta_standard_deviation = 30.983
+N_II_standard_deviation = 12.99717
+O_III_standard_deviation = 27.53118
+S_II_standard_deviation = 9.193647
+
+A_V_f_Ha = functions.a_v_map_creation(H_alfa, H_beta, H_beta_standard_deviation)
+A_V_f_Hb = functions.a_v_map_creation(H_beta, H_beta, H_beta_standard_deviation)
+A_V_f_n_ii = functions.a_v_map_creation(N_II, H_beta, H_beta_standard_deviation)
+A_V_f_o_iii = functions.a_v_map_creation(O_III, H_beta, H_beta_standard_deviation)
+A_V_f_s_ii = functions.a_v_map_creation(S_II, H_beta, H_beta_standard_deviation)
+
+k_lambda_values = []
+for length in emission_wavelengths:
+    k_lambda_values.append(functions.k_lambda(length))
+
+A_lambda_f_Ha = functions.a_lambda_calculation(A_V_f_Ha, k_lambda_values[0])
+A_lambda_f_Hb = functions.a_lambda_calculation(A_V_f_Hb, k_lambda_values[1])
+A_lambda_f_n_ii = functions.a_lambda_calculation(A_V_f_n_ii, k_lambda_values[2])
+A_lambda_f_0_iii = functions.a_lambda_calculation(A_V_f_o_iii, k_lambda_values[3])
+A_lambda_f_s_ii = functions.a_lambda_calculation(A_V_f_s_ii, k_lambda_values[4])
+A_lambda = [A_lambda_f_Ha, A_lambda_f_Hb, A_lambda_f_n_ii, A_lambda_f_0_iii, A_lambda_f_s_ii]
+
+print(A_lambda_f_Ha)
