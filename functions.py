a877322078da6766d8e5a8403bcadcc416f8cd16
ungespurv <radoslawwroblewski@yahoo.pl>
2023-01-16 10:44:05 +0100

    Further debugging

--- functions.py
+++ functions.py
@@ -7,24 +7,26 @@ from astropy.io import fits
 def a_v_map_creation(first_frequency_map, comparison_map, standard_deviation):
     """Scales a map to a certain frequency"""
     tmp_list = first_frequency_map
+    tmp_comparison = comparison_map
     for row in range(len(tmp_list)):
         for value in range(len(tmp_list[0])):
             if (
-                comparison_map[row][value] <= 0
-                or comparison_map[row][value] < 2 * standard_deviation
+                tmp_comparison[row][value] <= 0
+                or tmp_comparison[row][value] < 2 * standard_deviation
             ):
                 tmp_list[row][value] = 1
-            elif first_frequency_map[row][value] / comparison_map[row][value] < 2.86:
+            elif first_frequency_map[row][value] / tmp_comparison[row][value] < 2.86:
                 tmp_list[row][value] = 1
             else:
                 tmp_list[row][value] = (
-                    (first_frequency_map[row][value] / comparison_map[row][value])
+                    (first_frequency_map[row][value] / tmp_comparison[row][value])
                     / 2.86
                 ) ** 2.114
     # f_Ha = tmp_list # teraz jest f_Ha
     tmp_list = np.apply_along_axis(
         lambda x: 2.5 * np.log10(x), axis=-1, arr=tmp_list
     )  # -> A_V
+
     return tmp_list
 
 
@@ -70,6 +72,9 @@ def luminosity_from_flux(flux, redshift):
     tmp_map = flux
     distance = astropy.coordinates.Distance(z=redshift)
     distance = distance.to(u.cm).value
+    # 0.0136
+    # 19.500477
+    # 1.878712872817573e+26
     tmp_map = np.apply_along_axis(
         lambda x: 4.0 * np.pi * (distance**2) * x / (1 + redshift) * 10e-20,
         axis=0,
@@ -88,8 +93,8 @@ def calculate_sfr(l_map):
 def metallicity(l_ha, l_n_ii, l_s_ii):
     """Calculates a value of 12+log(O/H)"""
     y = np.log10(l_n_ii / l_s_ii) + 0.264 * (
-        np.log10(l_n_ii) / l_ha
-    )  # jeśli s_ii lub h_ii <0, to nie da sie wyliczyc metalicznosci
-    # desired_value = 8.77 + y + 0.45 * (y + 0.3) ** 5
-    desired_value = 8.77 + y
+        np.log10(l_n_ii / l_ha)
+    )  # jeśli s_ii lub h_ii <0, to nie da sie wyliczyc metalicznosci/
+    desired_value = 8.77 + y + 0.45 * (y + 0.3) ** 5
+    # desired_value = 8.77 + y
     return desired_value
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from astropy.io import fits
-from photutils.aperture import CircularAperture, aperture_photometry
 
 import functions
 
 # 332 x 335
 
 H_alfa = fits.getdata("AT2018cow_Ha.fits")  # 656.46 nm II
+H_alfa_original = fits.getdata("AT2018cow_Ha.fits")
 H_beta = fits.getdata("AT2018cow_Hb.fits")  # 486.1 nm I
 N_II = fits.getdata("AT2018cow_NII.fits")  # 648.4 nm II
 O_III = fits.getdata("AT2018cow_OIII.fits")  # 500.7nm I
@@ -16,55 +16,66 @@ H_beta_standard_deviation = 30.983
 N_II_standard_deviation = 12.99717
 O_III_standard_deviation = 27.53118
 S_II_standard_deviation = 9.193647
-print(H_alfa[193][176])
+# print(H_alfa[193][176])
 A_V_f_Ha = functions.a_v_map_creation(H_alfa, H_beta, H_beta_standard_deviation)
-print(A_V_f_Ha[193][176])
 # A_V_f_Hb = functions.a_v_map_creation(H_beta, H_beta, H_beta_standard_deviation)
 # A_V_f_n_ii = functions.a_v_map_creation(N_II, H_beta, H_beta_standard_deviation)
 # A_V_f_o_iii = functions.a_v_map_creation(O_III, H_beta, H_beta_standard_deviation)
 # A_V_f_s_ii = functions.a_v_map_creation(S_II, H_beta, H_beta_standard_deviation)
-functions.create_fits_file(A_V_f_Ha, "A_V_f_Ha.fits")
-
+# functions.create_fits_file(A_V_f_Ha, "A_V_f_Ha.fits")
+#
 k_lambda_values = []
 for length in emission_wavelengths:
     k_lambda_values.append(functions.k_lambda(length))
-print(k_lambda_values)
+# print(k_lambda_values)
 A_lambda_f_Ha = functions.a_lambda_calculation(A_V_f_Ha, k_lambda_values[0])
-A_lambda_f_Hb = functions.a_lambda_calculation(A_V_f_Ha, k_lambda_values[1])
+# A_lambda_f_Hb = functions.a_lambda_calculation(A_V_f_Ha, k_lambda_values[1])
 A_lambda_f_n_ii = functions.a_lambda_calculation(A_V_f_Ha, k_lambda_values[2])
-A_lambda_f_0_iii = functions.a_lambda_calculation(A_V_f_Ha, k_lambda_values[3])
+# A_lambda_f_0_iii = functions.a_lambda_calculation(A_V_f_Ha, k_lambda_values[3])
 A_lambda_f_s_ii = functions.a_lambda_calculation(A_V_f_Ha, k_lambda_values[4])
-
-functions.create_fits_file(A_lambda_f_Ha, "A_lambda_f_Ha.fits")
-# functions.create_fits_file(A_lambda_f_Hb, 'A_lambda_f_Hb.fits')
-# functions.create_fits_file(A_lambda_f_n_ii, 'A_lambda_f_n_ii.fits')
-# functions.create_fits_file(A_lambda_f_0_iii, 'A_lambda_f_0_iii.fits')
-# functions.create_fits_file(A_lambda_f_s_ii, 'A_lambda_f_s_ii.fits')
-
+#
+# functions.create_fits_file(A_lambda_f_Ha, "A_lambda_f_Ha.fits")
+# # functions.create_fits_file(A_lambda_f_Hb, 'A_lambda_f_Hb.fits')
+# # functions.create_fits_file(A_lambda_f_n_ii, 'A_lambda_f_n_ii.fits')
+# # functions.create_fits_file(A_lambda_f_0_iii, 'A_lambda_f_0_iii.fits')
+# # functions.create_fits_file(A_lambda_f_s_ii, 'A_lambda_f_s_ii.fits')
+#
 z = 0.0136  # https://www.astronomerstelegram.org/?read=11836
 L_Ha = functions.luminosity_from_flux(
     H_alfa * 10 ** (0.4 * A_lambda_f_Ha), z
 )  # Ha * 10^(0.4 * A_lambda_f_Ha)
-functions.create_fits_file(L_Ha, "L_Ha.fits")
-# L_Hb = functions.luminosity_from_flux(A_lambda_f_Hb, z)
+L_Ha_temporary = functions.luminosity_from_flux(
+    H_alfa_original * 10 ** (0.4 * A_lambda_f_Ha), z
+)
+# functions.create_fits_file(L_Ha, "L_Ha.fits")
+# # L_Hb = functions.luminosity_from_flux(A_lambda_f_Hb, z)
 L_N_II = functions.luminosity_from_flux(N_II * 10 ** (0.4 * A_lambda_f_n_ii), z)
-# L_O_III = functions.luminosity_from_flux(A_lambda_f_0_iii, z)
+# # L_O_III = functions.luminosity_from_flux(A_lambda_f_0_iii, z)
 L_S_II = functions.luminosity_from_flux(S_II * 10 ** (0.4 * A_lambda_f_s_ii), z)
 
-
+#
+#
 sfr_ha = functions.calculate_sfr(L_Ha)
-functions.create_fits_file(sfr_ha, "sfr_Ha.fits")
-
-log_o = functions.metallicity(L_Ha, L_N_II, L_S_II)
-functions.create_fits_file(log_o, "log_o.fits")
-# https://photutils.readthedocs.io/en/stable/aperture.html#performing-aperture-photometry
-aperture_1 = CircularAperture((148, 169), r=80.17)
-aperture_2 = CircularAperture((145, 175), r=7.24)
-phot_1 = aperture_photometry(sfr_ha, aperture_1)
-phot_2 = aperture_photometry(sfr_ha, aperture_2)
-aperture_l_ha = CircularAperture((148, 169), r=80.17)
-phot_3 = aperture_photometry(L_Ha, aperture_l_ha)
-phot_3 = aperture_photometry(L_Ha, aperture_l_ha)
-phot_3 = aperture_photometry(L_Ha, aperture_l_ha)
-phot_3 = aperture_photometry(L_Ha, aperture_l_ha)
-phot_3 = aperture_photometry(L_Ha, aperture_l_ha)
+# functions.create_fits_file(sfr_ha, "sfr_Ha.fits")
+#
+log_o = functions.metallicity(L_Ha_temporary, L_N_II, L_S_II)
+# print(H_alfa_original[193][176])
+# print(N_II[193][176])
+# print(S_II[193][176])
+print(L_Ha[193][176])
+print(L_Ha_temporary[193][176])
+print(L_N_II[193][176])
+print(L_S_II[193][176])
+print(log_o[193][176])
+# functions.create_fits_file(log_o, "log_o.fits")
+# # https://photutils.readthedocs.io/en/stable/aperture.html#performing-aperture-photometry
+# aperture_1 = CircularAperture((148, 169), r=80.17)
+# aperture_2 = CircularAperture((145, 175), r=7.24)
+# phot_1 = aperture_photometry(sfr_ha, aperture_1)
+# phot_2 = aperture_photometry(sfr_ha, aperture_2)
+# aperture_l_ha = CircularAperture((148, 169), r=80.17)
+# phot_3 = aperture_photometry(L_Ha, aperture_l_ha)
+# phot_3 = aperture_photometry(L_Ha, aperture_l_ha)
+# phot_3 = aperture_photometry(L_Ha, aperture_l_ha)
+# phot_3 = aperture_photometry(L_Ha, aperture_l_ha)
+# phot_3 = aperture_photometry(L_Ha, aperture_l_ha)
